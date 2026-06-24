aae0208fed17fe14dddc6a5d4dc8da542d6d8f0d
Doris-Lee97052388150 <donaldcookiex@outlook.com>
2026-06-25 00:32:00 +0800

    A New Commit! (2026-06-25 00:32:00)

--- batch.bat
+++ batch.bat
@@ -1,3 +1,7 @@
+@echo off
+:: 获取当前时间戳，格式为：YYYY-MM-DD HH:mm:ss
+for /f "delims=" %%i in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do set datetime=%%i
+
 git add .
-git commit -m "A New Commit!"
+git commit -m "A New Commit! (%datetime%)"
 git push origin main
